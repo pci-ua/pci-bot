98f7f7ad6af637d5a8f83e3e3abfc0fab42f523e
Mathieu <mathieu.toulon@free.fr>
2022-01-20 21:36:38 +0100

    new info poll command

---
+++ commands/info-poll.js
@@ -0,0 +1,28 @@
+const { isMemberBotAdmin } = require('../middleware/user.js');
+const { emoji } = require('../middleware/coin.js');
+async function main(interaction,bot) {
+
+	if( ! isMemberBotAdmin(interaction.member) ) {
+		interaction.reply({ content: 'Access denied', ephemeral: true});
+	}
+
+	let sondageID = interaction.options.getString('idsondage');
+	if( bot.sondages[ sondageID] ) {
+		const poll = bot.sondages[ sondageID ];
+		await interaction.reply( {content:
+`
+Question : ${ poll.question }
+Réponses :
+${ poll.reponses.map( k => `\t${k}` ).join`\n`}
+Type : ${ poll.type }
+
+Participant unique : ${ [... new Set(poll.answer.flat())].length }
+${ (poll.type == 'pari') ? `Total de ${emoji(interaction.member.guild)} investi : ${Object.values(poll.mise).reduce( (acc,now) => acc+now )}` : '' }
+
+`, ephemeral: true}
+		);
+	} else {
+		interaction.reply({ content: 'Sondage déjà clos ou inexistant ! ', ephemeral: true});
+	}
+}
+module.exports = main;
