738974e4bf8596322bfd314958af97e765dd95b5
Mathieu <mathieu.toulon@free.fr>
2022-01-26 21:31:34 +0100

    Clean up some mess

--- custom-log.js
+++ custom-log.js
@@ -8,11 +8,14 @@ const nowToLogString = () => {
 	return now.toLogString();
 };
 
-//
-console = {
-	old,
-	log: (... params) => old.log( '\033[1;96m', '📜', nowToLogString() , ... params , '\033[0m'),
-	warn: (... params) => old.warn( '\033[1;93m', '⚠', nowToLogString() , ... params , '\033[0m'),
-	error: (... params) => old.error( '\033[1;91m', 'ℯ', nowToLogString() , ... params , '\033[0m'),
+console.old = {};
 
-};
+// log
+console.old.log = console.log;
+console.log = (... params) => console.old.log( '\033[1;96m', '📜', nowToLogString() , ... params , '\033[0m');
+// warn
+console.old.warn = console.warn;
+console.warn = (... params) => old.warn( '\033[1;93m', '⚠', nowToLogString() , ... params , '\033[0m');
+// error
+console.old.error = console.error;
+console.error = (... params) => old.error( '\033[1;91m', 'ℯ', nowToLogString() , ... params , '\033[0m');
